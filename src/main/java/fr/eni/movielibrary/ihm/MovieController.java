afeb4830f8bdd6a0cae6f7e94c92292125424f1c
ejamin <esteban.jamin2022@campus-eni.fr>
2023-05-24 11:25:53 +0200

    init project

---
+++ src/main/java/fr/eni/movielibrary/ihm/MovieController.java
@@ -0,0 +1,15 @@
+package fr.eni.movielibrary.ihm;
+
+import java.util.List;
+
+import fr.eni.movielibrary.bo.Movie;
+
+public class MovieController {
+	public Movie findMovie(int idMovie) {
+		return null;
+	}
+	
+	public List<Movie> showAllMovies(){
+		return null;
+	}
+}
